f2c6934db8ef07b6b14b3a61dffbd736a5186d92
Aniket <anidhatrak@gmail.com>
2023-04-09 04:16:08 +0530

    login and logout urls

--- bharat_farm_x/bharat_farm_x/urls.py
+++ bharat_farm_x/bharat_farm_x/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('cover.urls'),name="cover")
+    path('',include('cover.urls'),name="cover"),
+    path('login/',auth_views.LoginView.as_view(template_name='html/login.html'), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='html/logout.html'), name='logout'),
 ]
